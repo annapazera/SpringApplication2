e9f710e01d94866149de9f8c4089ea1a637b26d9
Anna Pazera <anna.pazera@tlen.pl>
2016-08-09 11:14:07 +0200

    New classes added new web corrected main class

--- src/main/java/hello/NewGreetingController.java
+++ src/main/java/hello/NewGreetingController.java
@@ -1,12 +1,13 @@
 package hello;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by Anna Kacprzak on 2016-08-09.
- */
+ */@Controller
 public class NewGreetingController {
     @RequestMapping("/newgreeting")
     public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
